bc8c11efb1221e5d67111181fa81654d44a21509
kiquetal <kiquetal@gmail.com>
2025-11-18 11:50:38 -0300

    Refactor WebSocket service to improve method structure and formatting

--- src/main/webui/src/app/websocket.service.ts
+++ src/main/webui/src/app/websocket.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
-}
-  }
-    this.socket$.complete();
-  public close(): void {
-
-  }
-    this.socket$.next(message);
-  public sendMessage(message: any): void {
-
-  }
-    return this.socket$;
-    }
-      this.socket$ = webSocket(url);
-    if (!this.socket$ || this.socket$.closed) {
-  public connect(url: string): WebSocketSubject<any> {
-
-  private socket$: WebSocketSubject<any>;
-export class WebSocketService {
-})
-  providedIn: 'root'
-@Injectable({
-
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketService {
+  private socket$: WebSocketSubject<any>;
+
+  public connect(url: string): WebSocketSubject<any> {
+    if (!this.socket$ || this.socket$.closed) {
+      this.socket$ = webSocket(url);
+    }
+    return this.socket$;
+  }
+
+  public sendMessage(message: any): void {
+    this.socket$.next(message);
+  }
+
+  public close(): void {
+    this.socket$.complete();
+  }
+}
 
